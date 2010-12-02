b0b5482e8ada8519a061609e8e9f4ed920057b34
Danny <danny.glue@gmail.com>
2010-12-02 11:10:30 +1100

    Fixed size option issue. Added IE degradation.

--- jquery.noiser.js
+++ jquery.noiser.js
@@ -6,26 +6,27 @@
 			red: 0,
 			green: 0,
 			blue: 0,
-			size: 75
+			size: 60
 		}, options);
-		var canvas = $('<canvas>', {width: opts.size, height:opts.size })[0];
-		var ctx = canvas.getContext('2d');
-		
-		for ( x = 0; x < canvas.width; x++ ) {
-			for ( y = 0; y < canvas.height; y++ ) {
-				var number = Math.floor( Math.random() * opts.contrast );
-				var color_r = number + opts.red;
-				if (color_r > 255) {color_r = 255};
-				var color_g = number + opts.green;
-				if (color_g > 255) {color_g = 255};
-				var color_b = number + opts.blue;
-				if (color_b > 255) {color_b = 255};
+				
+		var canvas = $('<canvas width="' + opts.size + '" height="' + opts.size + '">')[0];
+		if (canvas.getContext) {
+			var ctx = canvas.getContext('2d');
+			for ( x = 0; x < canvas.width; x++ ) {
+				for ( y = 0; y < canvas.height; y++ ) {
+					var number = Math.floor( Math.random() * opts.contrast );
+					var color_r = number + opts.red;
+					if (color_r > 255) {color_r = 255};
+					var color_g = number + opts.green;
+					if (color_g > 255) {color_g = 255};
+					var color_b = number + opts.blue;
+					if (color_b > 255) {color_b = 255};
 
-				ctx.fillStyle = 'rgba(' + color_r + ',' + color_g + ',' + color_b + ',' + opts.opacity + ')';
-				ctx.fillRect(x, y, 1, 1);
+					ctx.fillStyle = 'rgba(' + color_r + ',' + color_g + ',' + color_b + ',' + opts.opacity + ')';
+					ctx.fillRect(x, y, 1, 1);
+				}
 			}
+			$(this).css({'background-image': 'url(' + canvas.toDataURL('image/png') + ')'});			
 		}
-
-		$(this).css({'background-image': 'url(' + canvas.toDataURL('image/png') + ')'});
 	};
 })(jQuery);
